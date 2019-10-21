fdb3184ea1e01080d22a7276fb50c1b49ee8cc1a
elias roussos <eliasisaiah@outlook.com>
2019-10-21 18:08:08 -0400

    -added paths to server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.use('/', '../public/index.html');
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/', path.join(__dirname, '..','public','index.html'));
 
 // Turn on that server!
 app.listen(PORT, () => {
