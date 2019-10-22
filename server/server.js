c2d0d2b0e3f61220274febef0bf9fab2ee9108cd
elias roussos <eliasisaiah@outlook.com>
2019-10-22 01:09:39 -0400

    -server.js is working

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 3002;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/', path.join(__dirname, '..','public','index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..','client','public','index.html'));
+});
 
 // Turn on that server!
 app.listen(PORT, () => {
