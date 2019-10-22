4dace561c9d137c82d27949405a1a7786ba25f1f
elias roussos <eliasisaiah@outlook.com>
2019-10-22 02:20:42 -0400

    slow progress towards react app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import about from './About';
+import Button from '@material-ui/core/Button';
+import 'typeface-roboto';
 import './App.css';
+import { Container } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="App-header">
+      <Container>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
+
+        </header>
+      </Container>
     </div>
   );
 }
