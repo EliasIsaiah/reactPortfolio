c6f3d9179d14e135d447414433d15c76a18e37d0
Elias Roussos <eliasIsaiah@outlook.com>
2019-10-30 14:44:49 -0400

    -changes to make the app run again

--- client/src/featuredProjects.tsx
+++ client/src/featuredProjects.tsx
@@ -2,67 +2,67 @@ import React from 'react';
 import axios from 'axios';
 
 
-interface Props {
-    error?: null | string,
-    isLoaded?: boolean,
-    repos?: string[],
-}
+// interface Props {
+//     error?: null | string,
+//     isLoaded?: boolean,
+//     repos?: string[],
+// }
 
-interface FeaturedProject {
+// interface FeaturedProject {
 
-}
+// }
 
-interface MyState {
-    error?: null | string,
-    isLoaded?: boolean,
-    repos?: string[],
-}
+// interface MyState {
+//     error?: null | string,
+//     isLoaded?: boolean,
+//     repos?: string[],
+// }
 
-class featuredProjects extends React.Component<Props, MyState> {
-    constructor(props: Props) {
-        super(props); 
-        this.state = {
-            error: null,
-            isLoaded: false,
-            repos: ["anything"],
-            // testString: "testString"
-        };
-    }
+// class featuredProjects extends React.Component<Props, MyState> {
+//     constructor(props: Props) {
+//         super(props); 
+//         this.state = {
+//             error: null,
+//             isLoaded: false,
+//             featuredProjects: ["anything"],
+//             // testString: "testString"
+//         };
+//     }
 
 
-    componentDidMount() {
-        axios.get(`https://api.github.com/users/eliasisaiah/repos`)
-            .then(res => {
-                const repos = res.data;
-                console.log(repos);
-                this.setState({
-                    isLoaded: true,
-                    repos: repos
-                })
-            },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
+//     componentDidMount() {
+//         axios.get(`https://api.github.com/users/eliasisaiah/repos`)
+//             .then(res => {
+//                 const repos = res.data;
+//                 console.log(repos);
+//                 this.setState({
+//                     isLoaded: true,
+//                     repos: repos
+//                 })
+//             },
+//                 (error) => {
+//                     this.setState({
+//                         isLoaded: true,
+//                         error
+//                     });
+//                 }
+//             )
+//     }
 
-    render() {
-        return (
+//     render() {
+//         return (
 
-            <ul>
-                {this.state.featuredProjects &&
-                    this.state.featuredProjects.map((featuredProject: any) => (
-                        <li key={featuredProject.id}>
-                            {featuredProject.name}
-                        </li>
-                    ))
-                }
-            </ul>
-        );
-    }
-}
+//             <ul>
+//                 {this.state.featuredProjects &&
+//                     this.state.featuredProjects.map((featuredProject: any) => (
+//                         <li key={featuredProject.id}>
+//                             {featuredProject.name}
+//                         </li>
+//                     ))
+//                 }
+//             </ul>
+//         );
+//     }
+// }
 
-export default featuredProjects;
+// export default featuredProjects;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 
 // Turn on that server!
 app.listen(PORT, () => {
-  console.log('App listening on port 3000');
+  console.log('App listening on port 3003');
 });
